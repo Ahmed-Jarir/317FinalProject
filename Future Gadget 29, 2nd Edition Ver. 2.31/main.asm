f013bb43e38df43b5584289ddd17bc6d5c2ced93
Ahmed-Jarir <ahmadjareer@gmail.com>
2024-01-06 21:10:32 +0300

    created isr for usart

--- Future Gadget 29, 2nd Edition Ver. 2.31/main.asm
+++ Future Gadget 29, 2nd Edition Ver. 2.31/main.asm
@@ -1,11 +1,10 @@
 
 ;*****************************************************************
 ;
-; File Name		: ‘Lab3-uart.asm'
-; Title			: Interrupt-Driven UART sample code
+; Title			: Interrupt-Driven USART sample code
 ; Date			: 11/20/2013
 ; Version		: 1.1
-; Target MCU	        : ATMega32
+; Target MCU	  : ATMega32
 ;
 ;*****************************************************************
 
@@ -16,7 +15,7 @@
 .equ	CPU_CLOCK	= 4000000
 .equ	BAUD_UBRR	= (CPU_CLOCK/(16*BAUD_RATE))-1
 
-.equ	MELOSIZE	= 200
+.equ	MELOSIZE	= 100
 
 .equ	Fck = CPU_CLOCK/64
 ; define register aliases
@@ -31,14 +30,18 @@ melody: .byte	MELOSIZE
 ; code segment
 .cseg
 .org    $000
-	jmp	reset		; $000 HW reset or watchdog
+	jmp		reset
 .org $012
-    jmp		tim1_ovf                 ; Timer1 overflow Handler
-/*.org $01A 
-	jmp USART_Receive
-*/; begin main code
+    jmp		tim1_ovf
+.org $01A 
+	jmp		USART_Receive
+//.org $01E
+//	jmp		USART_Receive
+
+; begin main code
 reset:
-	ldi 	TEMP, low(RAMEND)		; initialize stack pointer
+	; initialize stack pointer
+	ldi 	TEMP, low(RAMEND)
 	out		SPL, TEMP
 	ldi 	TEMP, high(RAMEND)
 	out 	SPH, TEMP
@@ -49,6 +52,8 @@ reset:
 	ldi	TEMP, high(BAUD_UBRR)
 	out	UBRRH, TEMP
 
+	ldi TEMP, $FF
+	out DDRB, TEMP
 	;set timer1 PWM mode
 	ldi   TEMP,(1<<WGM10)+(1<<COM1B1)
 	out   TCCR1A,TEMP								; 8 bit Fast PWM non-inverting (Fck/256)
@@ -59,51 +64,66 @@ reset:
 	ldi   TEMP,(1<<TOIE1)
 	out   TIMSK,TEMP
 
-	sbi UCSRB, TXEN			; enable transmitter
-	sbi UCSRB, RXEN			; enable receiver
-    
-	; set frame format: 8 data, 2 stop bits	ldi TEMP, (1 << URSEL)|(1 << USBS)| (3 << UCSZ0)	mov UCSRC, TEMP
-    sei					; enable interrupts
+	sbi UCSRB, TXCIE			; enable transmitter
+	sbi UCSRB, RXCIE			; enable receiver
+    sbi UCSRB, TXEN
+	sbi UCSRB, RXEN
+	; set frame format: 8 data, 2 stop bits	ldi TEMP, (1 << URSEL)|(1 << USBS)| (1 << UCSZ0)| (1 << UCSZ1)	mov UCSRC, TEMP
+    sei
 
 mloop:
-	rcall USART_Receive
+	//rcall USART_Receive_nisr
 	rjmp mloop
 
-USART_Receive:
+/*USART_Receive_nisr:
 	; Wait for data to be received
 	sbis UCSRA, RXC
-	rjmp USART_Receive
-	; Get and return received data from buffer
-	in TEMP, UDR
-	mov SENDCHAR, TEMP
-	rcall putchar
-	ret
+    rjmp    USART_Receive_nisr
+	
 
+	in		TEMP, UDR
+	inc		TEMP
+	mov		SENDCHAR, TEMP
+	rcall	putchar
+
+	ret*/
 ; subroutines
 putchar:
-        sbis    UCSRA, UDRE		; loop until USR:UDRE is 1
-        rjmp    putchar
-        out     UDR, SENDCHAR		; write SENTCHAR to transmitter buffer
-        ret
+    sbis    UCSRA, UDRE		; loop until USR:UDRE is 1
+    rjmp    putchar
+    out     UDR, SENDCHAR
+    ret
 
 ; Interrupt Handler
 
 tim1_ovf:
-	push   TEMP    				; Store temporary register
+	push   TEMP
 	in     TEMP,SREG
-	push   TEMP					; Store status register
+	push   TEMP
 
 	pop     TEMP
-	out     SREG, TEMP             ; Restore SREG
-	pop     TEMP                  ; Restore temporary register;
+	out     SREG, TEMP
+	pop		TEMP
 	reti
-/*USART_Receive:
+
+USART_Receive:
+	push	TEMP
+	in		TEMP,SREG
+	push	TEMP
+
 	; Wait for data to be received
-	sbis UCSRA, RXC
-	rjmp USART_Receive
-	; Get and return received data from buffer
-	in TEMP, UDR
-	mov SENDCHAR, TEMP
-	rcall putchar
+	ldi		TEMP, $08
+	out		PORTB, TEMP
+
+	in		TEMP, UDR
+	inc		TEMP
+	mov		SENDCHAR, TEMP
+	rcall	putchar
+	
+	ldi		TEMP, $10
+	out		PORTB, TEMP
+	
+	pop     TEMP
+	out     SREG, TEMP
+	pop     TEMP
 	reti
-*/
