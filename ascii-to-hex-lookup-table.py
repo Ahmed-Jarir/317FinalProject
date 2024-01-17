7de9bd6b26d7825089ab72f48a4904c890b50510
Ahmed-Jarir <ahmadjareer@gmail.com>
2024-01-17 16:40:23 +0300

    style: reformat ascii to hex lookup table

--- ascii-to-hex-lookup-table.py
+++ ascii-to-hex-lookup-table.py
@@ -1,4 +1,4 @@
-def ascii_to_hex(i):
+def ascii_to_int(i):
     ASCII_ZERO  = 0x30
     ASCII_NINE  = 0x39
     ASCII_A_UPR = 0x41
@@ -18,7 +18,10 @@ def ascii_to_hex(i):
 
     return INVALID
 
+def ascii_to_hex(i):
+    return f"0x{ascii_to_int(i):02X}"
+
 for i in range(0, 256, 2):
-    a = str(ascii_to_hex(i)).rjust(3)
-    b = str(ascii_to_hex(i + 1)).rjust(3)
+    a = ascii_to_hex(i)
+    b = ascii_to_hex(i + 1)
     print(f".db {a}, {b}")
